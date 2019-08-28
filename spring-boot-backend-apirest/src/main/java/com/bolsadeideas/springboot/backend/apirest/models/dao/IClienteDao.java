fc07c40848d49584138b28d0b1888a88bc61c2e0
eva.ortiz <evamariaob76@gmail.comz>
2019-08-28 21:07:49 +0200

    eliminados

--- spring-boot-backend-apirest/src/main/java/com/bolsadeideas/springboot/backend/apirest/models/dao/IClienteDao.java
+++
@@ -1,9 +0,0 @@
-package com.bolsadeideas.springboot.backend.apirest.models.dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.bolsadeideas.springboot.backend.apirest.models.entity.Cliente;
-
-public interface IClienteDao extends JpaRepository<Cliente, Long>{
-
-}
