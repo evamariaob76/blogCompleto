d49cd393203d28ed08661ecf8da2977f01f35caf
eva.ortiz <evamariaob76@gmail.comz>
2019-08-28 21:08:18 +0200

    eliminados

--- spring-boot-backendAnterior.blog/src/main/java/com/eva/blog/backend/model/dao/IComercioDao.java
+++
@@ -1,9 +0,0 @@
-package com.eva.blog.backend.model.dao;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.eva.blog.backend.models.entity.Comercio;
-
-public interface IComercioDao extends CrudRepository<Comercio, Long> {
-
-}
